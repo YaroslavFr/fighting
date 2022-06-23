95b5b09e6bc6ddc6cc3191541c8f09e604cc5cea
Ярослав Фролов <frolov.ya@office.lan>
2022-06-23 11:46:37 +0300

    final version

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,14 @@
 import './easy-toggler.min.js'
 import Swiper from 'swiper/bundle'
+import { Fancybox } from "@fancyapps/ui";
 
 document.addEventListener('DOMContentLoaded', () => {
-	"use strict"
+	"use strict";
 
-	const swiper = new Swiper('.swiper', {
-		loop: true,
-		slidesPerView: 1.2,
-		spaceBetween: 30,
-
-		// If we need pagination
-		pagination: {
-			el: '.swiper-pagination',
-		},
-
-		// Navigation arrows
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-
-		// Responsive breakpoints
-		breakpoints: {
-			// when window width is >= 768px
-			768: {
-				slidesPerView: 3,
-				spaceBetween: 20
-			},
-		}
-	});
+	let partners = document.querySelector("#partners");
+	if (window.innerWidth > 986) {
+		Fancybox.bind("[data-fancyboxx]", {
+			infinite: false,
+		});
+	}
 })
